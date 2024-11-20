129099df06278cbf55c879f3100b860e3e7b8088
Adrian <karatsumo@gmail.com>
2024-11-21 00:14:31 +0100

    init store

--- src/store/table/index.ts
+++ src/store/table/index.ts
@@ -2,14 +2,25 @@ import { defineStore } from 'pinia';
 
 export const useTableStore = defineStore('table', {
   state: () => ({
+    servicesResponse: {},
     dataTable: [],
-    tableHeaders: []
+    tableHeaders: [],
+    serviceName: ''
   }),
   actions: {
-    getTableData() {
-        
+    setTableData(payload) {
+      this.dataTable = payload     
+    },
+    setTableColumns(payload) {
+      this.tableHeaders = payload
+    },
+    init() {
+      const { data } = { data: {}}
+
+
     }
   },
   getters: {
+    getServiceName: (state) => state.serviceName
   },
 });
